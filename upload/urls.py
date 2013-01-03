ad8c24f7775986272688a767cb7819e901f92f7c
Radu Ioan Fericean <radu@fericean.ro>
2013-01-03 20:14:15 +0200

    pagination

--- upload/urls.py
+++ upload/urls.py
@@ -4,9 +4,10 @@ from models import Photo
 
 urlpatterns = patterns('',
     url(r'^$',  ListView.as_view(
-            queryset=Photo.objects.order_by('-pub_date')[:1],
+            queryset=Photo.objects.order_by('-pub_date'),
             context_object_name='latest_photo_list',
-            template_name='upload/index.html'),
+            template_name='upload/index.html',
+            paginate_by = 1),
         name='index'),
     url(r'^upload/$', 'upload.views.upload', name='upload'),
     url(r'^fullcaption/(?P<id>\d+)/$', 'upload.views.get_full_caption', name='get_full_caption'),
